9e4cd0663f6774d957f7aaf17d29e26f0b97b8c9
andlcoelho <andreluiscoelho@gmail.com>
2023-05-19 21:05:25 -0300

    Adicionando comentários ao código

--- 01_quiz_game.py
+++ 01_quiz_game.py
@@ -2,7 +2,7 @@ print("Bem vindos ao meu quiz para computador")
 
 jogando = input("Gostaria de iniciar o jogo?: ")
 
-if jogando.lower() != "Sim":
+if jogando != "sim":
     quit()
 
 print("OK então, vamos brincar :)")
@@ -10,28 +10,28 @@ print("OK então, vamos brincar :)")
 pontuacao = 0
 
 pergunta = input("Qual o significado de CPU?: ")
-if pergunta.lower() == "Unidade de processamento central":
+if pergunta == "unidade de processamento central":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
     print("Errou, ja era!")
 
 pergunta = input("Qual o significado de GPU?: ")
-if pergunta.lower() == "Unidade de processamento gráfico":
+if pergunta == "unidade de processamento gráfico":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
     print("Errou, ja era!")
 
 pergunta = input("Qual o significado de CPD?: ")
-if pergunta.lower() == "Central de Processamento de Dados":
+if pergunta == "central de Processamento de Dados":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
     print("Errou, ja era!")
 
 pergunta = input("Qual o significado de RAM?: ")
-if pergunta.lower() == "Random access memory":
+if pergunta == "random access memory":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:

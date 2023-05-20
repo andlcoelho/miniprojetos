ef0c33c5dc40a3f812b7383b80a5e7346ddd426f
andlcoelho <andreluiscoelho@gmail.com>
2023-05-19 21:11:25 -0300

    Adicionado comentarios sobre os códigos utilizados e ajustada a função lower() para as respostas

--- 01_quiz_game.py
+++ 01_quiz_game.py
@@ -1,37 +1,42 @@
+#Apresentação do programa
 print("Bem vindos ao meu quiz para computador")
 
+#primeira interação com o usuário
 jogando = input("Gostaria de iniciar o jogo?: ")
 
-if jogando != "sim":
+#checa a resposta do usuário, se quer iniciar o quiz ou não
+if jogando.lower() != "sim":
     quit()
 
 print("OK então, vamos brincar :)")
 
+#variável que vai receber a contagem de acertos
 pontuacao = 0
 
+#inicio das perguntas, interagindo com o usuário fazendo-o responder via terminal
 pergunta = input("Qual o significado de CPU?: ")
-if pergunta == "unidade de processamento central":
+if pergunta.lower() == "unidade de processamento central":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
     print("Errou, ja era!")
 
 pergunta = input("Qual o significado de GPU?: ")
-if pergunta == "unidade de processamento gráfico":
+if pergunta.lower() == "unidade de processamento gráfico":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
     print("Errou, ja era!")
 
 pergunta = input("Qual o significado de CPD?: ")
-if pergunta == "central de Processamento de Dados":
+if pergunta.lower() == "central de Processamento de Dados":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
     print("Errou, ja era!")
 
 pergunta = input("Qual o significado de RAM?: ")
-if pergunta == "random access memory":
+if pergunta.lower() == "random access memory":
     print("Acertou, mizeravi!")
     pontuacao += 1
 else:
